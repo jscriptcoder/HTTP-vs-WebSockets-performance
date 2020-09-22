5b11cb3ccb889d77da365ad035a229319ca8ae89
Francisco Ramos <jscriptcoder@gmail.com>
2020-09-22 16:26:21 +0200

    Added socketio server

---
+++ src/servers/socketio_server.py
@@ -0,0 +1,26 @@
+import eventlet
+import socketio
+import json
+import argparse
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+class HelloNamespace(socketio.Namespace):
+    def on_message(self, sid, data):
+        self.send({'hello': data['name']})
+
+# @sio.on('message', namespace='/hello')
+# def hello(sid, data):
+#     sio.send({'hello': data['name']}, namespace='/hello')
+
+sio.register_namespace(HelloNamespace('/hello'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--host', default='0.0.0.0')
+    parser.add_argument('-p', '--port', default=5000, type=int)
+
+    args = parser.parse_args()
+    print('Server starting at: ' + 'ws://{}:{}'.format(args.host, args.port))
+    eventlet.wsgi.server(eventlet.listen((args.host, args.port)), app)
